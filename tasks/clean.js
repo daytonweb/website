018857bedbb913d9165ea8256d5995384fb32e5c
Allen May <allenhmay@gmail.com>
2019-07-07 13:26:09 -0400

    Clear out folder content

--- tasks/clean.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(gulp, config, env) {
-  var del = require('del');
-
-  gulp.task('clean:tmp', del.bind(null, config.paths.temp));
-
-  gulp.task('clean:dest', del.bind(null, config.paths.dest));
-
-  gulp.task('clean', ['clean:tmp', 'clean:dest']);
-};
--- tasks/copy.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(gulp, config, env) {
-  gulp.task('copy', function() {
-    return gulp.src(config.paths.public + '/**/*')
-      .pipe(gulp.dest(config.paths.dest));
-  });
-};
